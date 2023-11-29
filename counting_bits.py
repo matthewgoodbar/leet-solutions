2d0c9477dfde9b3a20b221a0a4af1170519f4313
matthew <matthewgoodbar@gmail.com>
2023-11-29 15:51:51 -0800

    Counting bits improvement

--- counting_bits.py
+++ counting_bits.py
@@ -1,13 +1,13 @@
-# Given an integer n, return an array ans of length n + 1 such that for each i (0 <= i <= n), 
-# ans[i] is the number of 1's in the binary representation of i.
-
 class Solution:
-    def countBits(self, n: int) -> List[int]:
+    def countBits(self, n: int) -> list[int]:
         res = []
-        for num in range(n+1):
-            binString = format(num, "b")
-            ones = 0
-            for digit in binString:
-                if digit == "1": ones += 1
-            res.append(ones)
+        for i in range(n+1):
+            if i == 0:
+                res.append(0)
+            elif i == 1:
+                res.append(1)
+            elif i % 2 == 0:
+                res.append(res[i//2])
+            else:
+                res.append(res[i//2]+1)
         return res
