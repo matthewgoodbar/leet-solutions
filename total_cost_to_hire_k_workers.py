afd82af9c183aafa0eb6bf16b9e6b0948cbccde1
matthew <matthewgoodbar@gmail.com>
2023-12-05 12:32:57 -0800

    Total cost to hire k workers solution

---
+++ total_cost_to_hire_k_workers.py
@@ -0,0 +1,37 @@
+from collections import deque
+import heapq
+
+class Solution:
+    def totalCost(self, costs: list[int], k: int, candidates: int) -> int:
+        queue = deque(costs)
+        leftHeap = []
+        rightHeap = []
+        totalCost = 0
+
+        while queue and (len(leftHeap) < candidates and len(rightHeap) < candidates):
+            heapq.heappush(leftHeap, queue.popleft())
+            if queue:
+                heapq.heappush(rightHeap, queue.pop())
+
+        while k > 0:
+            if (leftHeap and not rightHeap) or (leftHeap and leftHeap[0] <= rightHeap[0]):
+                cost = heapq.heappop(leftHeap)
+                if queue:
+                    heapq.heappush(leftHeap, queue.popleft())
+            elif (rightHeap and not leftHeap) or (rightHeap and leftHeap[0] > rightHeap[0]):
+                cost = heapq.heappop(rightHeap)
+                if queue:
+                    heapq.heappush(rightHeap, queue.pop())
+            totalCost += cost
+            k -= 1
+        
+        return totalCost
+
+
+
+
+sol = Solution()
+costs = [57,33,26,76,14,67,24,90,72,37,30]
+k = 11
+candidates = 2
+print(sol.totalCost(costs, k, candidates))
