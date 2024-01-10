9725352380a14a47d31cd565b4e72f694d1dbd17
matthew <matthewgoodbar@gmail.com>
2024-01-10 14:03:05 -0800

    Partition equal subset sum solution

--- partition_equal_subset_sum.py
+++ partition_equal_subset_sum.py
@@ -1,19 +1,21 @@
-#ATTEMPT
-
 class Solution:
     def canPartition(self, nums: list[int]) -> bool:
         total = sum(nums)
         if total % 2 == 1:
             return False
         target = total // 2
+        dp = [[False for j in range(target+1)] for i in range(len(nums)+1)]
+        dp[0][0] = True
+        for i in range(1,len(nums)+1):
+            for j in range(target+1):
+                if j == 0 or dp[i-1][j]:
+                    dp[i][j] = True
+                elif j - nums[i-1] >= 0 and dp[i-1][j-nums[i-1]]:
+                    dp[i][j] = True
+                if j == target and dp[i][j]:
+                    return True
+        return dp[-1][-1]
 
-        def buildSubset(nums, target):
-            for num in nums:
-                if num == target:
-                    return True
-            for i in range(len(nums)):
-                if buildSubset(nums[:i]+nums[i+1:], target-nums[i]):
-                    return True
-            return False
-        
-        return buildSubset(nums, target)
+sol = Solution()
+nums = [1,5,11,5]
+sol.canPartition(nums)
